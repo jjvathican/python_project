fb2c75b5025cc689b3a7e0739071c968714e83c8
jerin jose <kuttyjerin@gmail.com>
2022-04-14 03:17:31 +0530

    Update check_net.py

--- Auto_wifi_project/check_net.py
+++ Auto_wifi_project/check_net.py
@@ -1,74 +1,129 @@
+#from asyncio import threads
+from concurrent.futures import thread
 import subprocess
 import requests
 from requests import get
 import time
 import _thread
 from pythonping import ping
+import urllib.request
+from socket import timeout
 # response_list = ping('8.8.8.8', size=1, count=10)
 # print(response_list.rtt_avg_ms)  
 import re
 import os
 hostname = "8.8.8.8" 
 
-url = ["https://www.google.com","https://www.google.com","https://www.google.com","https://www.google.com","https://www.google.com"]
-timeout = 5
+url = ["http://www.blogsearch.google.com/ping","http://googleping.com/","http://www.pingomatic.com"," http://www.pingler.com"]
+timeout = 3
+acount=len(url)-1
+print(acount)
 c=0
-a=5
+a=acount
 b=0
+threadstate=False
 change_state=1
 listis=[]
-def print_time(urllink):
+def print_time(urllink,arr):
+        global acount
         global a 
+        global threadstate
+        if(a<0 or arr<0):
+                
+                return
         # response = os.system("ping -c 1 " + hostname)
         # if response == 0:
         #         print(hostname, 'is up!')
         #         a=a+1
         # else:
         #         print(hostname, 'is down!')
+        url_stat=0
+       
+       
+
         try:
                 print("sending ping request")
                 response_list = ping('8.8.8.8', size=1, count=2)
-                print(response_list.rtt_avg_ms)     
+                print(response_list.rtt_avg_ms)
+                time.sleep(3)
+                # try:                
+                #         request = requests.get(urllink[a], timeout=timeout)   
+
+                # except (requests.ConnectionError, requests.Timeout) as exception:
+
+                #         print("No url connection.")  
+
                 if(response_list.rtt_avg_ms<1800):
-                                
-                        return
+                        a=acount  
+                        threadstate=False     
+                        return True
                 else:
-                        a=a-1
-                        if(a<0):
+                        threadstate=True
+                        print("pibg was long") 
+                        if(a<0 or arr<0):
                                 return
-                        time.sleep(5)
-                        print_time(urllink)
+                        
+                        _thread.start_new_thread( print_time, (urllink,arr-1,) )
+                        
+                        
         except:
                 print("no ping found")
-                a=a-1
-                if(a<0):
+                
+                
+
+        try:
+                print("arr:"+str(arr))
+                
+                url_stat=int(urllib.request.urlopen(urllink[arr],timeout=3).getcode())
+                print(urllink[arr])
+                print(url_stat)
+                if(url_stat==200):
+                        a=acount
+                        if arr==0:
+                                threadstate=False
+                        return True
+                else:
+                        a=a-1
                         return
-                time.sleep(5)
-                print_time(urllink)
-
-        # try:                
-        #         request = requests.get(urllink, timeout=timeout)          
-        #         #print("Connected to the Internet")
-        #         a=a+1       
-
-        # except (requests.ConnectionError, requests.Timeout) as exception:
-        #         print("No internet connection.")
+        # except(urllib.requests.ConnectionError, urllib.requests.Timeout) as exception:
+        
+        # except (HTTPError, URLError) as error:
+        #         print("HTTPERROR")
+        # except timeout:
+        #         print("TIMEOUTERROR")
+        except:
+                a=a-1
+                if arr==0:
+                        threadstate=False
+                # arr=len(url)-1  
+                print("url not reachable")
+                
+                return 
+                
+                
+                # print(url_stat)
+                # print(urllink[a])
+        
                 
 
 def connect_net():
         
-       
+        global a
+        global threadstate
         subprocess.run(["sudo","wpa_cli","-i","wlxd03745f41b5a","reconfigure"])
         time.sleep(.1)
         subprocess.run(["sudo","/usr/bin/ip","link","set","wlxd03745f41b5a","down"])
         time.sleep(.1)
         subprocess.run(["sudo","/usr/bin/ip","link","set","wlxd03745f41b5a","up"])
-        time.sleep(.1)
+        time.sleep(2)
         subprocess.run(["sudo","/usr/sbin/dhclient","-r","wlxd03745f41b5a"])
-        time.sleep(.1)
+        time.sleep(5)
         subprocess.run(["sudo","/usr/sbin/dhclient","-v","wlxd03745f41b5a"])
         time.sleep(5)
-        
+        subprocess.run(["sudo","/usr/sbin/dhclient","-v","wlxd03745f41b5a"])
+        time.sleep(5)
+        a=acount
+        threadstate=False
         # command = "sudo /usr/local/vpnclient/vpnclient stop"
         # subprocess.run(command, shell=True)
         # time.sleep(1)
@@ -122,28 +177,34 @@ while True:
         #                 print(i)
         #                 _thread.start_new_thread( print_time, (i,) )
                
-        restart=False    
+            
                 
-        if(a<1 or restart == True):
+        if(a<0):
                 while True:
+                        time.sleep(1)
                         try:
-                                c=c+1
-                                b=listis[c]
-
-                        except:
                                 print("Scanning available connections")
                                 ps = subprocess.Popen(("sudo", "/usr/sbin/iwlist" , "wlxd03745f41b5a","scan"), stdout=subprocess.PIPE)
                                 output = subprocess.check_output(("/usr/bin/grep", "ESSID:"), stdin=ps.stdout)
                                 ps.wait()
                                 print(output)
                                 listis=re.findall(r'"(.*?)"', str(output))
-                                c=0
+                                listis.sort()
                                 b=listis[c]
+                                c=c+1
+                        except:       
+                                c=0
+                                b=''
+                               
                         # change_state=1
                         
                         
-                                
-                        if(b == "Papa"):
+                        if(b == "DEVU"):
+                                print("Connecting to DEVU")
+                                subprocess.run(["sudo","/usr/bin/cp","/home/pi/wificonfig07.conf","/etc/wpa_supplicant/wpa_supplicant.conf"])
+                                connect_net()
+                                break        
+                        elif(b == "Papa"):
                                 print("Connecting to Papa")
                                 subprocess.run(["sudo","/usr/bin/cp","/home/pi/wificonfig03.conf","/etc/wpa_supplicant/wpa_supplicant.conf"])
                                 connect_net()
@@ -182,12 +243,26 @@ while True:
                                 
         
         try:
-                a=5
-                _thread.start_new_thread( print_time, (1,) )  
+                arr=len(url)-1
+                if(threadstate==False):
+                        _thread.start_new_thread( print_time, (url,arr,) )
+                        time.sleep(5)
+                       
+                        
         except:
+                a=a-1
                 print("No ping") 
-                restart=True   
-
+        pausecount=0   
+        print("runnning") 
+        while(threadstate==True):
+                print("paused") 
+                pausecount=pausecount+1
+                if pausecount>10:
+                        # a=-1
+                        print("loop exited")
+                        threadstate=False
+                        
+                time.sleep(1)  
         # try:
                 
         #         ip = get('https://api.ipify.org', timeout=timeout).text
@@ -198,7 +273,7 @@ while True:
         # except (requests.ConnectionError, requests.Timeout) as exception:
         #          print("exception occured")                        
 
-        time.sleep(3)
+        #time.sleep(3)
 
         # response = os.system("ping -c 1 " + hostname)
 
